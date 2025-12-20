d11346de8b32ed208ec6f74d7137956a6e0e50bd
shruthik shetty <shrutikshetty14@gmail.com>
2025-12-20 15:29:55 +0530

    fix jest setup

--- src/components/__test__/demo-component.test.tsx
+++ src/components/__test__/demo-component.test.tsx
@@ -1,12 +1,27 @@
 import { it, vi, expect, describe } from 'vitest';
 import DemoComponent from '../demo-component';
-import { render  , screen} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 describe('DemoComponent', () => {
   it('should render', () => {
-    render(
-      <DemoComponent onClick={vi.fn()} header={'Test header'} />,
-    );
+    render(<DemoComponent onClick={vi.fn()} header={'Test header'} />);
     expect(screen.getByText('Test header')).toBeInTheDocument();
+  });
+
+  it('should call onClick', () => {
+    const onClick = vi.fn();
+    render(<DemoComponent onClick={onClick} header={'Test header'} />);
+    screen.getByRole('button').click();
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+
+  // user fire event from user events this simulates user interaction / similar to actual user
+  it('should call onClick with userEvent', async () => {
+    const user = userEvent.setup();
+    const onClick = vi.fn();
+    render(<DemoComponent onClick={onClick} header={'Test header'} />);
+    await user.click(screen.getByRole('button'));
+    expect(onClick).toHaveBeenCalledTimes(1);
   });
 });
--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,2 +1,9 @@
 // vitest.setup.ts
 import '@testing-library/jest-dom/vitest';
+import { cleanup } from '@testing-library/react';
+import { afterEach } from 'vitest';
+
+// This forces Vitest to behave like Jest by clearing the DOM after every test
+afterEach(() => {
+  cleanup();
+});
