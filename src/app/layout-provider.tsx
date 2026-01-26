9b689af383d398e49a5de3ff73383b49e2b56d22
shruthik shetty <shrutikshetty14@gmail.com>
2026-01-26 13:57:09 +0530

    update providers

--- src/app/layout-provider.tsx
+++ src/app/layout-provider.tsx
@@ -4,11 +4,26 @@
  */
 import { ThemeProvider } from 'next-themes';
 import { APP_THEMES } from '../constants/config.constants';
+import { Toaster } from 'sonner';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { useState } from 'react';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
+/**
+ * This component wraps all the required providers for the app
+ */
 const AppLayoutProviders = ({ children }: { children: React.ReactNode }) => {
+  //initialize the query client
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <ThemeProvider attribute="class" defaultTheme="blurple" themes={APP_THEMES}>
-      {children}
+      <QueryClientProvider client={queryClient}>
+        <Toaster />
+        {children}
+        {/* Dev tools only active in dev mode */}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </ThemeProvider>
   );
 };
