67314f42caef88097f0ee185608ec76cb61544ec
shruthik shetty <shrutikshetty14@gmail.com>
2026-02-14 10:24:21 +0530

    set up form for login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,15 +9,38 @@ import {
   CardTitle,
 } from '@/src/components/ui/card';
 import { Input } from '@/src/components/ui/input';
-import { Label } from '@radix-ui/react-label';
-
+import {
+  loginDefaultValues,
+  loginSchema,
+  LoginSchema,
+} from '@/src/schema/login-schema';
+import { Controller, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from '@/src/components/ui/field';
 /**
  * Login page containing the login form
  */
 export default function Login() {
+  // create a login form using the login schema using react form hook
+  const { handleSubmit, control } = useForm<LoginSchema>({
+    mode: 'onChange',
+    resolver: zodResolver(loginSchema),
+    defaultValues: loginDefaultValues,
+  });
+
+  //handle login form submit
+  const handleLoginSubmit = (data: LoginSchema) => {
+    console.log(data);
+  };
+
   return (
     <div className="flex h-screen items-center justify-center">
-      <Card className="max-h-[90vh] max-w-[85vw] min-w-[70vw] md:min-w-[50vw] lg:min-w-[30vw]">
+      <Card className="border-muted-foreground max-h-[90vh] max-w-[85vw] min-w-[70vw] md:min-w-[50vw] lg:min-w-[30vw]">
         <CardHeader>
           <CardTitle className="text-center text-2xl font-bold">
             Welcome Back
@@ -27,25 +50,50 @@ export default function Login() {
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form className="flex flex-col gap-5">
-            <div className="flex flex-col gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input type="email" id="email" placeholder="enter your email" />
-            </div>
-            <div className="flex flex-col gap-2">
-              <Label htmlFor="password">Password</Label>
-              <Input
-                type="password"
-                id="password"
-                placeholder="enter your password"
+          <form
+            id="login-form"
+            className="flex flex-col gap-5"
+            onSubmit={handleSubmit(handleLoginSubmit)}
+          >
+            <FieldGroup>
+              <Controller
+                control={control}
+                name="email"
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel>Email</FieldLabel>
+                    <Input
+                      {...field}
+                      type="email"
+                      id="email"
+                      placeholder="enter your email"
+                    />
+                    {fieldState.error && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
               />
-            </div>
-            <Button
-              type="submit"
-              onClick={() => {
-                /* TODO: implement login */
-              }}
-            >
+              <Controller
+                control={control}
+                name="password"
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel>Password</FieldLabel>
+                    <Input
+                      {...field}
+                      type="password"
+                      id="password"
+                      placeholder="enter your password"
+                    />
+                    {fieldState.error && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+            </FieldGroup>
+            <Button type="submit" aria-label="sign-in">
               Sign in
             </Button>
           </form>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 'use client';
+import { useRouter } from 'next/navigation';
 import { ThemeSwitcher } from '../components/theme-switch';
 import { Button } from '../components/ui/button';
 
+// TODO page in dev
 /**
  * Home page of the app
  */
 export default function Home() {
+  const route = useRouter();
   return (
     <div className="m-auto flex flex-col items-center justify-center gap-2 text-center">
       <p>HOME PAGE</p>
@@ -14,6 +17,9 @@ export default function Home() {
         <ThemeSwitcher />
       </div>
       <Button>Button</Button>
+      <Button variant="default" onClick={() => route.push('/login')}>
+        Login
+      </Button>
     </div>
   );
 }
